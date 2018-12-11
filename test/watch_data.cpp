c95fe4b7cf12e387efc10a799f3870d5cade2c8c
wzyken1102 <zhiyu_wang@brown.edu>
2018-12-10 22:37:35 -0500

    modify the cell read part

--- test/watch_data.cpp
+++ test/watch_data.cpp
@@ -58,24 +58,47 @@ int main(){
     cout<<data->GetFieldData()->GetArray(0)->GetName()<<endl;
     
     cout<<"cell contains ";
-    double x[3];
-    vtkCell* cell0;
-    vtkIdType cellId=1;
-    double tol2=0.01;
-    int subId=1;
-    double pcoords[3];
-    double *weights;
-    data->FindCell(x, cell0, cellId, tol2, subId, pcoords, weights );
-    cout<<pcoords;
+//    double x[3];
+//    vtkCell* cell0;
+//    vtkIdType cellId=1;
+//    double tol2=0.01;
+//    int subId=1;
+//    double pcoords[3];
+//    double *weights;
+//    data->FindCell(x, cell0, cellId, tol2, subId, pcoords, weights );
+//    cout<<pcoords;
         //data->GetCellData()->Print(os);
-    vtkDataArray *dis_arr = data->GetFieldData()->GetArray(0);
+//    vtkDataArray *dis_arr = data->GetFieldData()->GetArray(0);
     cout<<data->GetFieldData()->GetArray(0)->GetName()<<endl;
-    double* u=data->GetPointData()->GetArray(2);
-    double* v=data->GetPointData()->GetArray(3);
-    double* w=data->GetPointData()->GetArray(4);
-        
+//    double* u=data->GetPointData()->GetArray(2);
+//    double* v=data->GetPointData()->GetArray(3);
+//    double* w=data->GetPointData()->GetArray(4);
+  
+    vtkCell* cell = data->GetCell(1);
+    cout<<"cell success"<<endl;
+    cout<< cell->GetPoints()->GetData()->GetNumberOfComponents()<<endl;
 
-    cout<<"number of array"<<data->GetPointData()->GetNumberOfArrays()<<endl;;
+    double* tup = cell->GetPoints()->GetData()->GetTuple(0);
+    cout<<tup[0]<<" "<<tup[1]<<" "<<tup[2]<<endl;
+    double* tup1 = cell->GetPoints()->GetData()->GetTuple(1);
+    cout<<tup1[0]<<" "<<tup1[1]<<" "<<tup1[2]<<endl;
+    double* tup2 = cell->GetPoints()->GetData()->GetTuple(2);
+    cout<<tup2[0]<<" "<<tup2[1]<<" "<<tup2[2]<<endl;
+    double* tup3 = cell->GetPoints()->GetData()->GetTuple(3);
+    cout<<tup3[0]<<" "<<tup3[1]<<" "<<tup3[2]<<endl;
+    double* tup4 = cell->GetPoints()->GetData()->GetTuple(4);
+    cout<<tup4[0]<<" "<<tup4[1]<<" "<<tup4[2]<<endl;
+    double* tup5 = cell->GetPoints()->GetData()->GetTuple(5);
+    cout<<tup5[0]<<" "<<tup5[1]<<" "<<tup5[2]<<endl;
+    double* tup6 = cell->GetPoints()->GetData()->GetTuple(6);
+    cout<<setprecision(10)<<tup6[0]<<" "<<tup6[1]<<" "<<tup6[2]<<endl;
+    double* tup7 = cell->GetPoints()->GetData()->GetTuple(7);
+    cout<<setprecision(10)<<tup7[0]<<" "<<tup7[1]<<" "<<tup7[2]<<endl;
+      
+    double* point0 = data->GetPoint(0);
+    cout<<"point location "<<point0[0]<<" "<<point0[1]<<" "<<point0[2]<<" "<<point0[3]<<" "<<point0[4]<<endl;
+    cout<<"number of array"<<data->GetPointData()->GetNumberOfArrays()<<endl;
+    
     data->GetPointData()->RemoveArray(1);  // delete the data that we do not want to visualize.
     data->GetPointData()->RemoveArray(1);
     data->GetPointData()->RemoveArray(1);
